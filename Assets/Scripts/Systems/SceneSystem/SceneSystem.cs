30bfb89c3542a24d6d57bd3999ca5543acff0438
Matthew Choi <blackrc3636@gmail.com>
2020-09-11 21:40:11 +0800

    1. Added scene1, scene2 2. Added Script SystemManager, SceneSystem 3. Added button in scene1, when click it, it would goto scene2

---
+++ Assets/Scripts/Systems/SceneSystem/SceneSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSystem : MonoBehaviour
+{
+    public static SceneSystem instance;
+    void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/Systems/SystemManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : MonoBehaviour
+{
+    public static SystemManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    public SceneSystem SceneSystem;
+}
