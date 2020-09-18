cfff4b8a6c65d7a015e546ce4186ae34c136183f
LAPTOP-GPEKOVT1\USER <0circo0@gmail.com>
2020-09-19 00:01:21 +0800

    Imported 2 audio clips, drafted a useless audio script

---
+++ Assets/Scripts/Systems/AudioSystem/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance; //singleton
+    
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Play(string name)
+    {
+        
+    }
+
+
+}
