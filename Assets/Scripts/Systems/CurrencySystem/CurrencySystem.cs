88090042e1ac3cf9d1c6cbd23f7616c1278e5ae1
LAPTOP-NQRUJ1F1\Tim <sls22142@gmail.com>
2020-10-22 19:42:12 +0800

    Currency System

--- Assets/Scripts/Systems/CurrencySystem/CurrencySystem.cs
+++ Assets/Scripts/Systems/CurrencySystem/CurrencySystem.cs
@@ -4,6 +4,87 @@ using UnityEngine;
 
 public class CurrencySystem : MonoBehaviour
 {
+    // Variable
+    public int money = 0;
+    public int diamond = 0;
+
+    
+    public static CurrencySystem Instance;
+    
+    
+    private void Awake()
+    {
+        Instance = this;
+    }
+  
+    public void AddMoney(int moneyToAdd)
+    {
+        money += moneyToAdd;
+    }
+
+    public void TakeMoney(int moneyToTake)
+    {
+        if (money < moneyToTake)
+        {
+            //no enough money
+        } 
+        else
+        {
+            money -= moneyToTake;
+        }
+    }
+
+    public void SetMoney(int moneyToSet)
+    {
+        if (moneyToSet < 0)
+        {
+            //wrong input
+        }
+        else
+        {
+            money = moneyToSet;
+        }
+    }
+
+    public void AddDiamond(int diamondToAdd)
+    {
+        diamond += diamondToAdd;
+    }
+
+    public void TakeDiamond(int diamondToTake)
+    {
+        if (diamond < diamondToTake)
+        {
+            //no enough diamond
+        }
+        else
+        {
+            diamond -= diamondToTake;
+        }
+    }
+
+    public void SetDiamond(int diamondToSet)
+    {
+        if (diamondToSet < 0)
+        {
+            //wrong input
+        }
+        else
+        {
+            diamond = diamondToSet;
+        }
+    }
+
+    public int GetMoney()
+    {
+        return money;
+    }
+
+    public int GetDiamond()
+    {
+        return diamond;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
